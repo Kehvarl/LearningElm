430ca78299ebe63c1e75f7cad56aef0e62e11424
Adam Corbett <kehvarl@kehvarl.com>
2019-10-27 15:21:31 -0700

    Created weighted-random generator Switched to using weighted-random generator.

--- src/RandomRoll.elm
+++ src/RandomRoll.elm
@@ -23,14 +23,23 @@ main =
 --MODEL
 
 
+type Face
+    = One
+    | Two
+    | Three
+    | Four
+    | Five
+    | Six
+
+
 type alias Model =
-    { dieFace : Int
+    { dieFace : Face
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model 1
+    ( Model One
     , Cmd.none
     )
 
@@ -41,7 +50,19 @@ init _ =
 
 type Msg
     = Roll
-    | NewFace Int
+    | NewFace Face
+
+
+roll : Random.Generator Face
+roll =
+    Random.weighted
+        ( 10, One )
+        [ ( 10, Two )
+        , ( 10, Three )
+        , ( 10, Four )
+        , ( 20, Five )
+        , ( 40, Six )
+        ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -49,7 +70,7 @@ update msg model =
     case msg of
         Roll ->
             ( model
-            , Random.generate NewFace (Random.int 1 6)
+            , Random.generate NewFace roll
             )
 
         NewFace newFace ->
@@ -74,6 +95,28 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text (String.fromInt model.dieFace) ]
+        [ h1 [] [ text (fromFace model.dieFace) ]
         , button [ onClick Roll ] [ text "Roll" ]
         ]
+
+
+fromFace : Face -> String
+fromFace face =
+    case face of
+        One ->
+            "1"
+
+        Two ->
+            "2"
+
+        Three ->
+            "3"
+
+        Four ->
+            "4"
+
+        Five ->
+            "5"
+
+        Six ->
+            "6"
